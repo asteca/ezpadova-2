da357eebc9f245c630cd17a63a94bd81c441b5f9
Gabriel <gabrielperren@gmail.com>
2020-12-08 19:37:45 -0300

    release version with CMDphotsysts merged

--- CMD.py
+++ CMD.py
@@ -7,29 +7,34 @@ directly from their website
 https://github.com/mfouesneau/ezpadova
 """
 
+
+import sys
 import requests
+from bs4 import BeautifulSoup
 import zlib
 import re
 import numpy as np
 from os.path import join, exists
 from os import makedirs
 
+webserver = 'http://stev.oapd.inaf.it'
+
 # Available sets of tracks (PARSEC + COLIBRI).
 map_models = {
-    'PAR12+37': ('parsec_CAF09_v1.2S', 'parsec_CAF09_v1.2S_S_LMC_08_web',
-                 'PARSEC v1.2S + COLIBRI S_37'),
-    'PAR12+35': ('parsec_CAF09_v1.2S', 'parsec_CAF09_v1.2S_S35',
-                 'PARSEC v1.2S + COLIBRI S_35'),
-    'PAR12+07': ('parsec_CAF09_v1.2S', 'parsec_CAF09_v1.2S_S07',
-                 'PARSEC v1.2S + COLIBRI S_07'),
-    'PAR12+16': ('parsec_CAF09_v1.2S', 'parsec_CAF09_v1.2S_NOV13',
-                 'PARSEC v1.2S + COLIBRI PR16'),
+    'PAR12+CS_37': ('parsec_CAF09_v1.2S', 'parsec_CAF09_v1.2S_S_LMC_08_web',
+                    'PARSEC v1.2S + COLIBRI S_37'),
+    'PAR12+CS_35': ('parsec_CAF09_v1.2S', 'parsec_CAF09_v1.2S_S35',
+                    'PARSEC v1.2S + COLIBRI S_35'),
+    'PAR12+CS_07': ('parsec_CAF09_v1.2S', 'parsec_CAF09_v1.2S_S07',
+                    'PARSEC v1.2S + COLIBRI S_07'),
+    'PAR12+CPR16': ('parsec_CAF09_v1.2S', 'parsec_CAF09_v1.2S_NOV13',
+                    'PARSEC v1.2S + COLIBRI PR16'),
     'PAR12+No': ('parsec_CAF09_v1.2S', 'no', 'PARSEC v1.2S + No')
 }
 
 __def_args__ = {
     "submit_form": (None, "Submit"),
-    "cmd_version": (None, "3.3"),
+    # "cmd_version": (None, "3.3"),
     "track_postagb": (None, "no"),
     "n_inTPC": (None, "10"),
     "eta_reimers": (None, "0.2"),
@@ -61,6 +66,16 @@ def main():
     # Read input parameters from file.
     gz_flag, evol_track, phot_syst, phot_syst_v, z_range, a_vals =\
         read_params()
+
+    # Read optional argument
+    try:
+        _arg = sys.argv[1]
+    except IndexError:
+        _arg = 'No'
+    if _arg.lower() == 'list':
+        systemsList()
+        return
+
     # Ages to add to the files.
     ages = np.arange(*map(float, a_vals))
     # Add the largest value if it is not included
@@ -68,13 +83,8 @@ def main():
         ages = np.append(ages, float(a_vals[1]))
     ages = 10. ** ages
 
-    # Map isochrones set selection to proper name.
-    iso_sys = {
-        'PAR12+No': 'parsec12_No', 'PAR12+16': 'parsec12_16',
-        'PAR12+07': 'parsec12_07', 'PAR12+35': 'parsec12_35',
-        'PAR12+37': 'parsec12_37'}
-    # Sub-folder where isochrone files will be stored.
-    sub_folder = iso_sys[evol_track] + '_' + phot_syst + '/'
+    # Sub-folder where isochrone files will be stored. Notice the lower-case
+    sub_folder = phot_syst.lower() + '/'
 
     # If the sub-folder doesn't exist, create it before moving the file.
     full_path = 'isochrones/' + sub_folder
@@ -83,20 +93,33 @@ def main():
 
     print('\nQuery CMD using: {}.'.format(map_models[evol_track][-1]))
     print("Requesting isochrones in the '{}' system.".format(phot_syst))
-
     track_parsec, track_colibri = map_models[evol_track][:-1]
+
+    # Update parameters in dictionary
+    __def_args__['output_gzip'] = (None, gz_flag)
+    __def_args__['track_parsec'] = (None, track_parsec)
+    __def_args__['track_colibri'] = (None, track_colibri)
+    __def_args__['isoc_lagelow'] = (None, a_vals[0])
+    __def_args__['isoc_lageupp'] = (None, a_vals[1])
+    __def_args__['isoc_dlage'] = (None, a_vals[2])
+    __def_args__['photsys_file'] = (
+        None, 'tab_mag_odfnew/tab_mag_{0}.dat'.format(phot_syst))
+    __def_args__['photsys_version'] = (None, phot_syst_v)
+
     # Run for given range in metallicity.
     for i, metal in enumerate(z_range):
 
         print('\nz = {} ({}/{})'.format(metal, i + 1, len(z_range)))
 
-        # Define isochrones' parameters
-        par_dict = isoch_params(
-            gz_flag, a_vals, metal, track_parsec, track_colibri, phot_syst,
-            phot_syst_v)
+        # Update metallicity parameter
+        par_dict = __def_args__.copy()
+        par_dict['isoc_zlow'] = (None, str(metal))
 
         # Query the service
-        data = __query_website(par_dict)
+        data, c = __query_website(par_dict, phot_syst)
+
+        if i == 0:
+            filterLambaOmega(c, evol_track, full_path)
 
         # Add ages to each isochrone
         data = addAge(data, ages)
@@ -109,7 +132,125 @@ def main():
         with open(file_name, 'w') as f:
             f.write(data)
 
-    print('\nAll done.')
+    print('\nAll done!')
+
+
+def systemsList():
+    """
+    """
+    r = requests.get('http://stev.oapd.inaf.it/cgi-bin/cmd')
+    soup = BeautifulSoup(r.content, "lxml")
+    systs = soup.find_all("select")[0]
+
+    replc = ["""<option value="tab_mag_odfnew/tab_mag_""",
+             """<option selected="" value="tab_mag_odfnew/tab_mag_""",
+             "</option>", "<i>", "</i>", "<sub>", "</sub>"]
+
+    print("\n{:<40} {}".format("System's ID", "System's name"))
+    print("------------------------------------------------------")
+    for s in systs:
+        sr = str(s)
+        for _ in replc:
+            sr = sr.replace(_, "")
+        sr = [_.strip() for _ in sr.split(""".dat">""")]
+        if sr[0] != "":
+            print("{:<40} {}".format(sr[0], sr[1]))
+
+    print("\nAll systems listed")
+
+
+def __query_website(d, phot_syst):
+    """
+    Communicate with the CMD website.
+    """
+
+    print('  Interrogating {0}...'.format(webserver))
+    c = requests.post(webserver + '/cgi-bin/cmd', files=d).text
+    aa = re.compile('output\d+')
+    fname = aa.findall(c)
+    if len(fname) > 0:
+
+        url = '{0}/tmp/{1}.dat'.format(webserver, fname[0])
+        print('  Downloading data...{0}'.format(url))
+        r = requests.get(url)
+
+        typ = gzipDetect(r.content)
+        if typ == "gz":
+            print("  Compressed 'gz' file detected")
+            rr = zlib.decompress(bytes(r.content), 15 + 32).decode('ascii')
+        else:
+            rr = r.text
+        return rr, c
+    else:
+
+        error_msg = ("Photometric system {} still not available among YBC "
+                     "tables.").format(phot_syst)
+        if error_msg in c:
+            raise RuntimeError(error_msg)
+        else:
+            print(c)
+            err_i = c.index("errorwarning")
+            txt = c[err_i + 17:err_i + 17 + 100]
+            print('\n' + txt.split("<br>")[0].replace("</b>", ""), '\n')
+            raise RuntimeError('FATAL: Server Response is incorrect')
+
+
+def filterLambaOmega(c, evol_track, full_path):
+    """
+    Extract filters, lambdas, and omegas data
+    """
+    # Extract filters, lambdas, and omegas data
+    aa = re.compile('Filter.*<th>&lambda')
+    fname = aa.findall(c)
+    # In CMD v3.2 apparently all filters have a 'mag' added.
+    filters = [
+        _.split('</td>')[0] + 'mag' for _ in fname[0].split('<td>')][1:]
+    aa = re.compile('lambda.*omega')
+    fname = aa.findall(c)
+    lambdas = [_.split('</td>')[0] for _ in fname[0].split('<td>')][1:]
+    aa = re.compile('omega.*lambda')
+    fname = aa.findall(c)
+    omegas = [_.split('</td>')[0] for _ in fname[0].split('<td>')][1:]
+
+    # Store in file.
+    with open(full_path + "/filterslambdas.dat", 'w') as f:
+        f.write(evol_track + "     " + "    ".join(filters + lambdas + omegas))
+
+
+def gzipDetect(data):
+    """
+    Detect potential compressed "gz" file.
+    """
+    gzchars = b"\x1f\x8b\x08"
+
+    if data[:len(gzchars)] == gzchars:
+        return "gz"
+
+    return None
+
+
+def addAge(data, ages):
+    """
+    The new format in CMD v3.2 does not include the commented line with the
+    'Age' value, and the logAge column is rounded to 3 decimal places so
+    this value can not be taken from there.
+
+    Add this line back to each age for each metallicity file.
+    """
+
+    data = data.split('\n')
+
+    # Indexes of "# Zini" comments
+    idx = []
+    for i, line in enumerate(data):
+        if line.startswith("# Zini"):
+            idx.append(i)
+
+    # Insert new comments in their proper positions
+    for i, j in enumerate(idx):
+        data.insert(j + i, "# Age = {:.6E} yr".format(ages[i]))
+
+    return "\n".join(data)
 
 
 def read_params():
@@ -152,101 +293,11 @@ def read_params():
                 if reader[0] == 'AR':
                     a_vals = reader[1:4]
 
+    if evol_track not in map_models.keys():
+        raise ValueError("Evolutionary track '{}' is invalid".format(
+            evol_track))
+
     return gz_flag, evol_track, phot_syst, phot_syst_v, z_range, a_vals
-
-
-def isoch_params(
-    gz_flag, a_vals, metal, track_parsec, track_colibri, phot_syst,
-        phot_syst_v):
-    """
-    Define parameters in dictionary
-    """
-    d = __def_args__.copy()
-
-    d['output_gzip'] = (None, gz_flag)
-
-    d['track_parsec'] = (None, track_parsec)
-    d['track_colibri'] = (None, track_colibri)
-
-    d['isoc_zlow'] = (None, str(metal))
-    logt0, logt1, dlogt = a_vals
-    d['isoc_lagelow'] = (None, logt0)
-    d['isoc_lageupp'] = (None, logt1)
-    d['isoc_dlage'] = (None, dlogt)
-
-    # d['imf_file'] = (None, map_imfs[imf_sel])
-    d['photsys_file'] = (
-        None, 'tab_mag_odfnew/tab_mag_{0}.dat'.format(phot_syst))
-    d['photsys_version'] = (None, phot_syst_v)
-
-    return d
-
-
-def __query_website(d):
-    """
-    Communicate with the CMD website.
-    """
-
-    webserver = 'http://stev.oapd.inaf.it'
-    print('  Interrogating {0}...'.format(webserver))
-    c = requests.post(webserver + '/cgi-bin/cmd', files=d).text
-    aa = re.compile('output\d+')
-    fname = aa.findall(c)
-    if len(fname) > 0:
-
-        url = '{0}/tmp/{1}.dat'.format(webserver, fname[0])
-        print('  Downloading data...{0}'.format(url))
-        r = requests.get(url)
-
-        typ = gzipDetect(r.content)
-        if typ == "gz":
-            print("  Compressed 'gz' file detected")
-            rr = zlib.decompress(bytes(r.content), 15 + 32).decode('ascii')
-        else:
-            rr = r.text
-        return rr
-    else:
-        print(c)
-        err_i = c.index("errorwarning")
-        txt = c[err_i + 17:err_i + 17 + 100]
-        print('\n' + txt.split("<br>")[0].replace("</b>", ""), '\n')
-        raise RuntimeError('FATAL: Server Response is incorrect')
-
-
-def gzipDetect(data):
-    """
-    Detect potential compressed "gz" file.
-    """
-    gzchars = b"\x1f\x8b\x08"
-
-    if data[:len(gzchars)] == gzchars:
-        return "gz"
-
-    return None
-
-
-def addAge(data, ages):
-    """
-    The new format in CMD v3.2 does not include the commented line with the
-    'Age' value, and the logAge column is rounded to 3 decimal places so
-    this value can not be taken from there.
-
-    Add this line back to each age for each metallicity file.
-    """
-
-    data = data.split('\n')
-
-    # Indexes of "# Zini" comments
-    idx = []
-    for i, line in enumerate(data):
-        if line.startswith("# Zini"):
-            idx.append(i)
-
-    # Insert new comments in their proper positions
-    for i, j in enumerate(idx):
-        data.insert(j + i, "# Age = {:.6E} yr".format(ages[i]))
-
-    return "\n".join(data)
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,22 +1,29 @@
 ezPADOVA-2
 ==========
 
-A python package that allows you to download PADOVA/PARSEC isochrones directly
-from the [CMD](http://stev.oapd.inaf.it/cgi-bin/cmd) website.
+A python package to download PARSEC isochrones from the [CMD](http://stev.oapd.inaf.it/cgi-bin/cmd) website. Simplified fork of the original [ezpadova](https://github.com/mfouesneau/ezpadova) code by [Morgan Fouesneau](https://github.com/mfouesneau).
 
-Simplified fork of the original [ezpadova](https://github.com/mfouesneau/ezpadova) code by [Morgan Fouesneau](https://github.com/mfouesneau).
+
 
 Requirements
 ------------
 
-Uses the `requests`, and `numpy` packages. The latest release can be downloaded [here](https://github.com/asteca/ezpadova-2/releases).
+Install the requirements in a `conda` environment with:
+
+    $ conda install numpy requests beautifulsoup4 lxml
+
+
 
 Usage
 -----
 
 Evolutionary tracks, photometric system, metallicity and age ranges are set via
-the input data file `in_params.dat`.
+the input data file `in_params.dat`. To run the script, simply use:
 
-A list of the proper IDs for the CMD photometric systems ('System ID') can be obtained with the [CMDphotsysts](https://github.com/asteca/CMDphotsysts/) code. You can also just read the latest fetched names [here](https://github.com/asteca/CMDphotsysts/blob/master/CMD_systs_NEW.dat).
+    $ python CMD.py
+
+A list of the proper IDs to identify the CMD photometric systems ('System ID') can be obtained calling the script as:
+
+    $ python CMD.py list
 
 The downloaded files are stored in a folder that can be pasted inside the `isochrones/` sub-folder of the `ASteCA` package.
